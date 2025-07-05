77fde096f420be86200d0f33c7c46a03d4b73e1d
neih3 <hiencastoo@gmail.com>
2025-07-05 23:48:42 +0700

    2

--- installer2.sh
+++ installer2.sh
@@ -18,15 +18,15 @@ echo "=== Cấp quyền cho thư mục Splunk ==="
 chown -R $SPLUNK_USER:$SPLUNK_GROUP $SPLUNK_DIR
 
 echo "=== Khởi động lần đầu để chấp nhận license và đặt mật khẩu ==="
-$SPLUNK_DIR/bin/splunk start --accept-license --seed-passwd "$SPLUNK_PASSWORD"
+sudo -u $SPLUNK_USER $SPLUNK_DIR/bin/splunk start --accept-license --seed-passwd "$SPLUNK_PASSWORD"
 
 echo "=== Dừng Splunk lại để thiết lập boot-start ==="
-$SPLUNK_DIR/bin/splunk stop
+sudo -u $SPLUNK_USER $SPLUNK_DIR/bin/splunk stop
 
 echo "=== Thiết lập Splunk khởi động cùng hệ thống ==="
-$SPLUNK_DIR/bin/splunk enable boot-start --user $SPLUNK_USER
+sudo -u $SPLUNK_USER $SPLUNK_DIR/bin/splunk enable boot-start --user $SPLUNK_USER
 
 echo "=== Khởi động lại Splunk ==="
-$SPLUNK_DIR/bin/splunk start
+sudo -u $SPLUNK_USER $SPLUNK_DIR/bin/splunk start
 
 echo "=== Cài đặt hoàn tất ==="
